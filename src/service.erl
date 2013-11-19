1b70993b9920dff09c12c53c8711a682e5a33503
Michael Truog <mjtruog@gmail.com>
2013-11-18 16:44:22 -0800

    initial commit

---
+++ src/service.erl
@@ -0,0 +1,89 @@
+%-*-Mode:erlang;coding:utf-8;tab-width:4;c-basic-offset:4;indent-tabs-mode:()-*-
+% ex: set ft=erlang fenc=utf-8 sts=4 ts=4 sw=4 et:
+%%%
+%%%------------------------------------------------------------------------
+%%% @doc
+%%% ==Service Behaviour==
+%%% A minimal behaviour for creating CloudI internal services.
+%%% @end
+%%%
+%%% BSD LICENSE
+%%% 
+%%% Copyright (c) 2013, Michael Truog <mjtruog at gmail dot com>
+%%% All rights reserved.
+%%% 
+%%% Redistribution and use in source and binary forms, with or without
+%%% modification, are permitted provided that the following conditions are met:
+%%% 
+%%%     * Redistributions of source code must retain the above copyright
+%%%       notice, this list of conditions and the following disclaimer.
+%%%     * Redistributions in binary form must reproduce the above copyright
+%%%       notice, this list of conditions and the following disclaimer in
+%%%       the documentation and/or other materials provided with the
+%%%       distribution.
+%%%     * All advertising materials mentioning features or use of this
+%%%       software must display the following acknowledgment:
+%%%         This product includes software developed by Michael Truog
+%%%     * The name of the author may not be used to endorse or promote
+%%%       products derived from this software without specific prior
+%%%       written permission
+%%% 
+%%% THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND
+%%% CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES,
+%%% INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+%%% OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+%%% DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR
+%%% CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+%%% SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+%%% BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+%%% SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+%%% INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
+%%% WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+%%% NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+%%% OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH
+%%% DAMAGE.
+%%%
+%%% @author Michael Truog <mjtruog [at] gmail (dot) com>
+%%% @copyright 2013 Michael Truog
+%%% @version 1.3.1 {@date} {@time}
+%%%------------------------------------------------------------------------
+
+-module(service).
+-author('mjtruog [at] gmail (dot) com').
+
+-include("service_req.hrl").
+
+-type service_req() :: #service_req{}.
+-export_type([service_req/0]).
+
+-callback service_config() ->
+    cloudi_service_api:service_internal() |
+    cloudi_service_api:service_proplist().
+
+-callback service_init(Args :: list(),
+                       Prefix :: cloudi_service:service_name_pattern(),
+                       Dispatcher :: cloudi_service:dispatcher()) ->
+    {'ok', State :: any()} |
+    {'stop', Reason :: any()} |
+    {'stop', Reason :: any(), State :: any()}.
+
+-callback service_request(ServiceReq :: #service_req{},
+                          State :: any(),
+                          Dispatcher :: cloudi_service:dispatcher()) ->
+    {'reply', Response :: cloudi_service:response(), NewState :: any()} |
+    {'reply', ResponseInfo :: cloudi_service:response_info(),
+     Response :: cloudi_service:response(), NewState :: any()} |
+    {'forward', NextServiceReq :: #service_req{}, NewState :: any()} |
+    {'noreply', NewState :: any()} |
+    {'stop', Reason :: any(), NewState :: any()}.
+
+-callback service_info(Request :: any(),
+                       State :: any(),
+                       Dispatcher :: cloudi_service:dispatcher()) ->
+    {'noreply', NewState :: any()} |
+    {'stop', Reason :: any(), NewState :: any()}.
+
+-callback service_terminate(Reason :: any(),
+                            State :: any()) ->
+    'ok'.
+
