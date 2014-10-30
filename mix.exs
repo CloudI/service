c6904b8e6638ecb51a6c2ac33abd8ec84669b953
Michael Truog <mjtruog@gmail.com>
2014-10-30 16:10:49 -0700

    Update hex package.

---
+++ mix.exs
@@ -0,0 +1,28 @@
+defmodule Service do
+  use Mix.Project
+
+  def project do
+    [app: :service,
+     version: "1.4.0-rc.4",
+     language: :erlang,
+     description: description,
+     package: package,
+     deps: deps]
+  end
+
+  defp deps do
+    [{:cloudi_core, "~> 1.4.0-rc.4"}]
+  end
+
+  defp description do
+    "Erlang/Elixir Cloud Framework Service Behavior"
+  end
+
+  defp package do
+    [files: ~w(src doc include rebar.config README.markdown),
+     contributors: ["Michael Truog"],
+     licenses: ["BSD"],
+     links: %{"Website" => "http://cloudi.org",
+              "GitHub" => "https://github.com/CloudI/service"}]
+   end
+end
